e542bcdb3696bdb7b8d7316bc6b410db4bf1e5bb
shazzad098 <shazzad920@gmail.com>
2025-09-26 12:58:44 +0600

    fixed vercel.json file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: './', // <--- এই লাইনটি যোগ করুন
   plugins: [tailwindcss()],
   server: { port: 3000 },
   build: {
